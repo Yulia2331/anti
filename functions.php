faf778cc9ae36fb52ce9bb428f8f7b7dbb8c11d5
Egor <68809735+ZOrtanin@users.noreply.github.com>
2022-12-05 10:43:08 +0300

    правки рутов

--- functions.php
+++ functions.php
@@ -247,7 +247,7 @@ function theme_name_scripts() {
 }
 
 function custom_login_page() {
-	$new_login_page_url = home_url( '/auth/' ); // new login page
+	$new_login_page_url = home_url( '/antinorma.com/auth/' ); // new login page
 	global $pagenow;
 	if( $pagenow == "wp-login.php" && $_SERVER['REQUEST_METHOD'] == "GET") {
 		wp_redirect($new_login_page_url);
